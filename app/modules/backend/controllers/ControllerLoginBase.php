d7fe0272e0f0bf92f2746f73c90bd55bd83aba6e
yihaocompany <574574@qq.com>
2018-04-16 17:26:39 +0800

    后台登陆 LAYOUT

---
+++ app/modules/backend/controllers/ControllerLoginBase.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Dldh\Modules\Backend\Controllers;
+use Phalcon\Mvc\View;
+
+class loginBaseController extends ControllerBase
+{
+
+
+}
+
---
+++ app/modules/frontend/controllers/ErrorController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/4/16/016
+ * Time: 16:50
+ */
+
+namespace Dldh\Modules\Frontend\Controllers;
+
+
+class ErrorController
+{
+
+}
